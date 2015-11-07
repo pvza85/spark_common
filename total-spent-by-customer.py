698f36c706d22c3e12d992c16c317187a7f06b39
Payam <pvza85@gmail.com>
2015-11-07 17:43:36 +0300

    first spark code

---
+++ total-spent-by-customer.py
@@ -0,0 +1,20 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("SpendByCustomer")
+sc = SparkContext(conf = conf)
+
+def extractCustomerPricePairs(line):
+    fields = line.split(',')
+    return (int(fields[0]), float(fields[2]))
+
+input = sc.textFile("file:///home/payam/workspace/spark/customer-orders.csv")
+mappedInput = input.map(extractCustomerPricePairs)
+totalByCustomer = mappedInput.reduceByKey(lambda x, y: x + y)
+
+flipped = totalByCustomer.map(lambda (x,y):(y,x))
+totalByCustomerSorted = flipped.sortByKey()
+
+
+results = totalByCustomerSorted.collect();
+for result in results:
+    print result
